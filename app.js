9b4cdd644af0bdd395381f75029b378a3c01bb10
Sabina Vargas Huamán <sabinavargash@gmail.com>
2025-09-22 14:52:06 -0500

    Se agrega función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 let arregloNombres = [];
+
+function agregarAmigo(){
+    let obtenerNombre = document.querySelector('.input-name').value.trim();
+    if (!arregloNombres.includes(obtenerNombre)){
+        arregloNombres.push(obtenerNombre);
+        actualizarAmigos(arregloNombres)
+    }
+    limpiarInput();
+
+}
