8515c3636ec41d92e5ba0be60ec83cbcbe8e0dae
Sabina Vargas Huamán <sabinavargash@gmail.com>
2025-09-22 14:54:47 -0500

    Se agrega función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ function agregarAmigo(){
 function limpiarInput(){
     document.querySelector('.input-name').value = '';
 }
+
+
+function actualizarAmigos(){
+    let elementoLista = document.querySelector('.name-list');
+    elementoLista.innerHTML ='';
+    arregloNombres.forEach(elemento => {
+    let item = document.createElement('li');
+    item.textContent = elemento;
+    elementoLista.appendChild(item);
+    });
+}
